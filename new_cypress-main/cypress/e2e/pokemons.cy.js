3ced399e39bcef689189dffe7d02d9a58acb75cc
Lizzssss <153381759+Lizzssss@users.noreply.github.com>
2024-01-15 11:21:02 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/pokemons.cy.js
@@ -0,0 +1,18 @@
+describe('Покупка аватара Покемоны', function () {
+   it('Тест на покупку нового аватара для тренера', function () {
+        cy.visit('https://pokemonbattle.me/');
+        cy.get(':nth-child(1) > .auth__input').type('kozyreva_elizaveta@inbox.ru');
+        cy.get('#password').type('vHM2mZLuFgTq7xy');
+        cy.get('.auth__button').click();
+        cy.get('.header__btns > [href="/shop"]').click();
+        cy.get('.shop__list > li').not('.feature-empty').children('.shop__button').eq(0).click();
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4276400030848452');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1027');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('Elizaveta Kozyreva');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').click();
+    })
+})
---
+++ new_cypress-main/cypress/e2e/staya.cy.js
@@ -0,0 +1,43 @@
+
+describe('Тестирование staya', function () {
+    
+    it('Проваливаюсь в категорию поводки и ищу совпадение с названием Heatwave', function () {
+        cy.visit('https://staya.dog/');
+        cy.contains('input', 'Поводки').click();
+        cy.contains('Heatwave');
+        cy.end();
+         })
+
+     it('search something', function () {
+        cy.visit('https://staya.dog/');
+        })
+})
+
+
+
+describe('Автотесты на форму логина и пароля', function () {
+
+          it('Позитивный кейс авторизации', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        })
+
+          it('Проверка логики восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        })
+
+
+
+
+})
